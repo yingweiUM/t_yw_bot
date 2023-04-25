b87b9e9caf85ce15d0ce8e3352d1425679803087
CHUA YING WEI <131639908+yingweiUM@users.noreply.github.com>
2023-04-25 22:22:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,16 +23,18 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def my_async_function():
+    application = ApplicationBuilder().token(tele_token).build()
+    start_handler = CommandHandler('start', start)
+    hello_handler = CommandHandler('hello', hello)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(start_handler)
+    application.add_handler(hello_handler)
+    application.add_handler(echo_handler)
+    application.run_polling()
 
-
-application = ApplicationBuilder().token(tele_token).build()
-    
-start_handler = CommandHandler('start', start)
-hello_handler = CommandHandler('hello', hello)
-echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    
-application.add_handler(start_handler)
-application.add_handler(hello_handler)
-application.add_handler(echo_handler)
-    
-application.run_polling()
+if __name__ == '__main__':
+    try:
+        asyncio.run(my_async_function())
+    except KeyboardInterrupt:
+        pass
