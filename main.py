26cc3c1f15a5dd2b584fecdd70e5c8cbfc0c8d56
CHUA YING WEI <131639908+yingweiUM@users.noreply.github.com>
2023-04-25 22:17:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,15 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(tele_token).build()
+
+application = ApplicationBuilder().token(tele_token).build()
     
-    start_handler = CommandHandler('start', start)
-    hello_handler = CommandHandler('hello', hello)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+start_handler = CommandHandler('start', start)
+hello_handler = CommandHandler('hello', hello)
+echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     
-    application.add_handler(start_handler)
-    application.add_handler(hello_handler)
-    application.add_handler(echo_handler)
+application.add_handler(start_handler)
+application.add_handler(hello_handler)
+application.add_handler(echo_handler)
     
-    application.run_polling()
+application.run_polling()
